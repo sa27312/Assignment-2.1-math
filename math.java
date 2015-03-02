51c4d6ae1bdea0da001604c61c5a297ba50da8f0
unknown <sa27312@an098083-ct107v.nvstu.edu>
2015-03-02 14:12:45 -0500

    add math

---
+++ math.java
@@ -0,0 +1,12 @@
+public class math{
+	public static void main(String[] args){
+		System.out.println(1/2);
+		System.out.println(1%2);
+		System.out.println(1.0/2);
+		System.out.println(5+7/2);
+		System.out.println("Beat" + 's' + "Army");
+		System.out.println(6 + 13/5 - 35%3);
+		System.out.println(3.5*(5/4));
+		System.out.println((3.5*5)/4);
+	}
+}
